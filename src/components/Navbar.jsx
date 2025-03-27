3621f1af582dedaaf58aba8de5b6b03154655872
Nalithdev <Ethan.delcroix@outlook.fr>
2025-03-27 15:21:47 +0100

    Merge conflict

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,18 +66,18 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="../../pants.html"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                About
+                Pantalon
               </a>
             </li>
             <li>
               <a
-                href="#"
+                href="../../t-shirt.html"
                 className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Services
+                T-shirt
               </a>
             </li>
             <li>
